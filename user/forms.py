e6a846b94121369e7262147b51adf93b5a5e0196
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-01-28 21:15:02 +0300

    Created a base class for user creation forms

--- user/forms.py
+++ user/forms.py
@@ -1,35 +1,79 @@
 from django.forms import ModelForm
-from .models import PrepaidMeterUser
+from django.contrib.auth import get_user_model
 from django import forms
+from passwords.fields import PasswordField
 from .acc_types import ADMIN, MANAGER
 
 admin_acc_choices = (
     (MANAGER, "Manager"),
 )
 
+User = get_user_model()
+
+
+class CreateUserBaseForm(ModelForm):
+    password = PasswordField()
+    password2 = PasswordField(label="Confirm Password")
+    field_order = ["first_name", "last_name", "email", "phone_no", "address", "acc_type"]
+
+    def clean_password2(self):
+        password = self.cleaned_data["password"]
+        password2 = self.cleaned_data["password2"]
+        if password != password2:
+            raise forms.ValidationError("Passwords must be the same")
+
+        return password
+
+    def save(self):
+        first_name = self.cleaned_data["first_name"]
+        last_name = self.cleaned_data["last_name"]
+        phone_no = self.cleaned_data["phone_no"]
+        email = self.cleaned_data["email"]
+        password = self.cleaned_data["password"]
+        address = self.cleaned_data["address"]
+        acc_type = int(self.cleaned_data["acc_type"]) # Convert to Integer
+
+        user = None
+        if acc_type == ADMIN:
+            user = User.objects.create_admin(phone_no, first_name, last_name, address, password=password, email=email)
+        elif acc_type == MANAGER:
+            user = User.objects.create_manager(phone_no, first_name, last_name, address, password=password, email=email)
+
+        return user
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+
 # Admins are not allowed to create SuperAdmin accounts, So this form is used for admin accounts
-class AdminCreateUserForm(ModelForm):
-    acc_type = forms.PositiveIntegerField(max_legnth=1, choices=admin_acc_choices)
+
+
+class AdminCreateUserForm(CreateUserBaseForm):
+    
+    acc_type = forms.ChoiceField(label="Account Type", choices=admin_acc_choices)
+    
     class Meta:
-        model = PrepaidMeterUser
+        model = User
         fields = ["first_name", "last_name", "email", "phone_no", "address"]
 
 
-super_admin_choices = (
+super_admin_acc_choices = (
     (ADMIN, "Admin"),
     (MANAGER, "Manager"),
 )
 
 # The super admin can create admins and managers, Use super admin account choices
-class SuperAdminCreateUserForm(ModelForm):
-    acc_type = forms.PositiveIntegerField(max_legnth=1, choices=super_admin_acc_choices)
+class SuperAdminCreateUserForm(CreateUserBaseForm):
+    acc_type = forms.ChoiceField(label="Account Type", choices=super_admin_acc_choices)
     class Meta:
-        model = PrepaidMeterUser
+        model = User
         fields = ["first_name", "last_name", "email", "phone_no", "address"]
+
 
 
 class EditUserForm(ModelForm):
     class Meta:
-        model = PrepaidMeterUser
+        model = User
         # The only fields that are allowed to be edited
         fields = ["first_name", "last_name", "email", "phone_no", "address"]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,83 @@
 from django.shortcuts import render
-
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.forms import PasswordResetForm
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib import messages
+from django.http import Http404, HttpResponse
+from meter.utils import is_admin, is_super_admin, is_admin_or_super_admin
+from user.forms import SuperAdminCreateUserForm, EditUserForm, AdminCreateUserForm
+from user.acc_types import SUPER_ADMIN, ADMIN, MANAGER
 # Create your views here.
+
+User = get_user_model()
+
+@user_passes_test(is_admin_or_super_admin)
+def list_users(request):
+    user_type = request.user.acc_type
+    if user_type == SUPER_ADMIN:
+        # List admin manger accounts
+        pass
+    elif user_type == ADMIN:
+        # List only manager accounts
+        pass
+    
+
+@user_passes_test(is_admin_or_super_admin)
+def edit_user(request, pk):
+    if request.method == "POST":
+        # process form and save updated fields
+        edit_user_form = EdituserForm(request.POST)
+        try:
+            user = User.objects.get(id=pk)
+        except:
+            return Http404("Error occured")
+
+        if edit_user_form.is_valid():
+            pass
+        
+    # render form
+    return render(request, "user/edit_user.html.development", {"form": EditUserForm()}) 
+
+@user_passes_test(is_admin_or_super_admin)
+def create_user(request):
+    user_type = SUPER_ADMIN or request.user.acc_type
+    designation = "Manager"
+    if request.method == "POST":
+        # Process the form and create user
+        if user_type == SUPER_ADMIN:
+            create_user_form = SuperAdminCreateUserForm(request.POST)
+        elif user_type == ADMIN:
+            create_user_form = AdminCreateUserForm(request.POST)
+
+        if create_user_form.is_valid():
+            user = create_user_form.save()
+            if user.acc_type == ADMIN:
+                designation = "Admin"
+            # render the form with a success flash message attached
+            messages.success(request, "%s: %s %s has been created successfully" %(designation, user.first_name, user.last_name))
+            
+        # Rerender the form 
+        return render(request, 'user/create_user.html.development', {"form": create_user_form})
+
+    else:
+        # Render the AdminCreateForm or the SuperAdminCreateForm depending on the account type
+        if user_type == SUPER_ADMIN:
+            return render(request, 'user/create_user.html.development' ,{"form": SuperAdminCreateUserForm()})
+        elif user_type == ADMIN:
+            return render(request, 'user/creaet_user.html.development', {"form": AdminCreateUserForm()})
+
+        
+
+@user_passes_test(is_admin_or_super_admin)
+def revoke_password(request, pk):
+    pass
+
+
+def reset_password_request(request):
+    pass
+
+@login_required
+def profile(request):
+    return render(request, "user/profile.html.development")
