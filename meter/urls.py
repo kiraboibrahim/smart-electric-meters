72b9311242f53ea82cf7b8b99d8fbd7a3a109cff
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-04-04 09:19:08 +0300

    Meter endpoints (routes) definition complete

--- meter/urls.py
+++ meter/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from .views import list_meters, create_meter, edit_meter, delete_meter, get_token
 
 
 urlpatterns = [
+    path("", list_meters, name="list-meters"),
+    path("<int:pk>/edit", edit_meter, name="edit_meter"),
+    path("create", create_meter, name="create_meter"),
+    path("<int:pk>/delete", delete_meter, name="delete_meter"),
+    path("<int:pk>/get-token", get_token, name="get-token"),
 ]
