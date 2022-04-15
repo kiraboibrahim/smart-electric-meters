e36f4357bdbcbe3bfc03e9b986a1884864d70ffe
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-04-15 15:23:37 +0300

    Registered Transaction app in the admin application

--- transaction/admin.py
+++ transaction/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from transaction.models import Transaction
 
 # Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Transaction, TransactionAdmin)
