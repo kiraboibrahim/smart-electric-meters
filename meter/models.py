83ea6cad8ead85fada1d1a9e5a99f6f2a263a908
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-04-16 11:55:58 +0300

    Created a separate entity for meter type and manufacturer, Enforced integrity on models (on deletion)

--- meter/models.py
+++ meter/models.py
@@ -5,13 +5,27 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 
+class MeterType(models.Model):
+    percentage_charge = models.PositiveIntegerField()
+    fixed_charge = models.PositiveIntegerField()
+    label = models.CharField(max_length=255)
+
+
+class Manufacturer(models.Models):
+    # The name of the manufacturer is strongly coupled to api classes in the api.py, so developers and admins
+    # collaborate
+    name = models.CharField("Manufacturer's Name", max_length=255)
+
+    
 class Meter(models.Model):
     # Meter numbers have a length of 11
     meter_no = models.CharField("Meter Number", max_length=11, unique=True)
-    manufacturer = models.CharField("Meter Manufacturer", max_length=255)
-    # If a manager is deleted the meter will not be deleted, instead it will be retained
-    manager = models.ForeignKey(get_user_model(), null=True, blank=True, on_delete=models.PROTECT)
-
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT)
+    # If a manager is deleted, the meter owner will be set NULL ie disowning all from the manager
+    manager = models.ForeignKey(get_user_model(), null=True, blank=True, models.SET_NULL)
+    # Prevent deletion of meter types if there are still child rows referencing it
+    meter_type = models.ForeignKey(MeterType, on_delete=models.PROTECT)
+    
     def __str__(self):
         return self.meter_no
 
@@ -20,7 +34,8 @@ class Meter(models.Model):
 class TokenHistory(models.Model):
     # From the decision reached, the name field is not necessary, and thus it is allowed to have NULL values
     name = models.CharField("Purchaser's Name", max_length=255, null=True)
-    user = models.ForeignKey(get_user_model(), null=True, on_delete=models.PROTECT)
+    # When a user who bought a token is deleted from the database, then all child rows in this table will be set to NULL
+    user = models.ForeignKey(get_user_model(), null=True, models.SET_NULL)
     token_no = models.PositiveIntegerField(unique=True)
     phone_no = models.CharField(max_length=10)
     # Amount of money paid
@@ -29,7 +44,9 @@ class TokenHistory(models.Model):
     num_token_units = models.CharField(max_length=255)
     # Automatically populate with the current date and time 
     purchased_at = models.DateTimeField(auto_now_add=True)
-    # Meter for which token was generated
+    # Meter for which token was generated, I don't see any reason why one would delete a meter that is already
+    # in production, but if that happens and there are still any child rows(token history)  still referencing
+    # a meter, prevent deletion 
     meter = models.ForeignKey(Meter, on_delete=models.PROTECT)
 
 
