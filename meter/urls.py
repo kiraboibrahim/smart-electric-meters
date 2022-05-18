63d684a55eca15ce0bcf6b6ff79a1298dafa02fc
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-05-18 13:31:34 +0300

    Added hashid to meter routes to obscure meter ids

--- meter/urls.py
+++ meter/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path, include
-from .views import list_meters, create_meter, edit_meter, delete_meter, get_token
+from django.urls import path, include, register_converter
+from .views import list_meters, register_meter, edit_meter, delete_meter, buy_token
+from user.utils import HashIdConverter
+
+register_converter(HashIdConverter, "hashid")
 
 
 urlpatterns = [
-    path("", list_meters, name="list-meters"),
-    path("<int:pk>/edit", edit_meter, name="edit_meter"),
-    path("create", create_meter, name="create_meter"),
-    path("<int:pk>/delete", delete_meter, name="delete_meter"),
-    path("<int:pk>/get-token", get_token, name="get-token"),
+    path("", list_meters, name="list_meters"),
+    path("<hashid:pk>/edit", edit_meter, name="edit_meter"),
+    path("register", register_meter, name="register_meter"),
+    path("<hashid:pk>/delete", delete_meter, name="delete_meter"),
+    path("<hashid:pk>/buy_token", buy_token, name="buy_token"),
 ]
