fca490ddcd71a109defe8761c88c105ba8e1b6d4
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-07-31 17:17:51 +0300

    refactor: Redirect to login view when user visit the index page

--- prepaid_meters_token_generator_system/urls.py
+++ prepaid_meters_token_generator_system/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", auth_views.LoginView.as_view(template_name="user/login.html.development"), name="home"),
+    path("", RedirectView.as_view(pattern_name="login"), name="index"),
     url(r'^admin/', admin.site.urls),
     path("meters/", include("meter.urls")), 
     path("users/", include("user.urls"))
