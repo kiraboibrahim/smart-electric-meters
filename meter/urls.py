2ac068999645b5fc875e3171695ce190d3ad1286
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-07-31 17:14:36 +0300

    refactor: Prefix views with a namespace instead of importing all views by name

--- meter/urls.py
+++ meter/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path, include, register_converter
-from meter.views import MeterListView, MeterCreateView, MeterEditView, MeterCategoryCreateView, MeterManufacturerCreateView, BuyTokenView, delete_meter, unlink_meter
+from meter import views as meter_views
 from user.utils import HashIdConverter
 
 register_converter(HashIdConverter, "hashid")
 
 
 urlpatterns = [
-    path("", MeterListView.as_view(), name="list_meters"),
-    path("register", MeterCreateView.as_view(), name="register_meter"),
-    path("buy-token", BuyTokenView.as_view(), name="buy_token"),
-    path("<hashid:pk>/edit", MeterEditView.as_view(), name="edit_meter"),
-    path("<hashid:pk>/delete", delete_meter, name="delete_meter"),
-    path("<hashid:pk>/buy-token", BuyTokenView.as_view(), name="buy_token"),
-    path("<hashid:pk>/unlink", unlink_meter, name="unlink_meter"),
-    path("manufacturers/register", MeterManufacturerCreateView.as_view(), name="register_meter_manufacturer"),
-    path("categories/register", MeterCategoryCreateView.as_view(), name="register_meter_category"), 
+    path("", meter_views.MeterListView.as_view(), name="list_meters"),
+    path("add", meter_views.MeterCreateView.as_view(), name="add_meter"),
+    path("recharge", meter_views.RechargeMeterView.as_view(), name="recharge_meter"),
+    path("<hashid:pk>/edit", meter_views.MeterEditView.as_view(), name="edit_meter"),
+    path("<hashid:pk>/deactivate", meter_views.deactivate_meter, name="deactivate_meter"),
+    path("<hashid:pk>/recharge", meter_views.RechargeMeterView.as_view(), name="recharge_meter"),
+    path("manufacturers/add", meter_views.MeterManufacturerCreateView.as_view(), name="add_meter_manufacturer"),
+    path("categories/add", meter_views.MeterCategoryCreateView.as_view(), name="add_meter_category"), 
 ]
