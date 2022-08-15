56ece71c51bfb7c3c991b44e135506937ef76ab9
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-08-15 22:20:30 +0300

    fix: Redesign the FiltersListView and SearchListView generic views in such a way that they are independent ie Donot inherit FiltersListView in SearchListView

--- prepaid_meters_token_generator_system/utils/forms.py
+++ prepaid_meters_token_generator_system/utils/forms.py
@@ -2,7 +2,23 @@
 from django import forms
 from django.core.exceptions import ImproperlyConfigured
 
-class BaseFiltersForm(forms.Form):
+class BaseForm(forms.Form):
+    
+     def get_request_parameter_string(self):
+        request_parameter_items = []
+        
+        if self.is_valid():    
+            for field in self.cleaned_data:
+                field_value = self.cleaned_data[field]
+                if field_value is None or field_value == '':
+                    continue
+                request_parameter_items.append("{}={}".format(field, field_value))
+
+        return "&".join(request_parameter_items)
+
+
+    
+class BaseFiltersForm(BaseForm):
 
     def get_filters(self):
         filters = {}
@@ -12,23 +28,24 @@ class BaseFiltersForm(forms.Form):
                     continue
                 filters[field] = value
         return filters
+    
 
 
-class BaseSearchForm(forms.Form):
+class BaseSearchForm(BaseForm):
     q = forms.CharField(required=True) # Query string
     model_search_field = None
 
-    def get_filters(self):
-        filters = {}
+    def get_predicates(self):
+        predicates = {}
         if self.is_valid():
             query_string = self.cleaned_data.get("q")
             if isinstance(self.model_search_field, forms.ChoiceField):
                 model_search_field = self.cleaned_data.get("model_search_field")
                 model_search_field = "{}__contains".format(model_search_field)
-                filters[model_search_field] = query_string
+                predicates[model_search_field] = query_string
             elif isinstance(self.model_search_field, str):
                 model_search_field = "{}__contains".format(self.model_search_field)
-                filters[model_search_field] = query_string
+                predicates[model_search_field] = query_string
             else:
                 raise ImproperlyConfigured("model_search_field can either be a string or django.forms.ChoiceField")
-        return filters
+        return predicates
--- prepaid_meters_token_generator_system/utils/views.py
+++ prepaid_meters_token_generator_system/utils/views.py
@@ -15,95 +15,107 @@ class FiltersListView(ListView):
     def get_filters(self):
         filters = {}
         filters_form_class = self.get_filters_form_class()
+        filters_form = filters_form_class(self.request.GET) # Bind the form to the GET Parameters
 
-        if self.request.GET:
-            filters_form = filters_form_class(self.request.GET) # Bind the form to the GET Parameters
-            self.filters_form = filters_form
-            filters = filters_form.get_filters()
-            self.filters = filters
+        self.filters_form = filters_form
+
+        filters = filters_form.get_filters()
+
+        self.filters = filters
+
         return filters
 
     def get_queryset(self):
         filters = self.get_filters()
         return super(FiltersListView, self).get_queryset().filter(**filters)
 
-    def get_filters_string(self):
-        filters_list = []
-        filters_string = ""
-        if self.filters_form:
-            filters_form_fields = self.filters_form.declared_fields
-            for key, value in self.request.GET.items():
-                # Exclude GET parameters that are not part of the filters form fields
-                if key in filters_form_fields:
-                    filters_list.append("{}={}".format(key, value))
-        filters_string = "&".join(filters_list)
-        return filters_string
-                    
+
+    def get_filters_form(self):
+        if self.filters_form is None:
+            # Form has not been bounded, return Unbound Form
+            filters_form_class = self.get_filters_form_class()
+            filters_form  = filters_form_class()
+        else:
+            filters_form = self.filters_form
+        return filters_form
+
+    def get_request_parameter_string(self):
+        filters_form = self.get_filters_form()
+        request_parameter_string = filters_form.get_request_parameter_string()
+        return request_parameter_string
         
     def get_context_data(self, **kwargs):
         context = super(FiltersListView, self).get_context_data(**kwargs)
-        if self.filters_form is None:
-            # Form has not been bounded, return Unbound Form
-            context["filters_form"] = self.get_filters_form_class()
-        else:
-            context["filters_form"] = self.filters_form
-            
-        context["filters_string"] = self.get_filters_string
+        filters_form = self.get_filters_form()
+        request_parameter_string = self.get_request_parameter_string()
+
+        context["filters_form"] = filters_form
+        context["request_parameter_string"] = request_parameter_string
+        
         return context
 
 
-class SearchListView(FiltersListView):
+class SearchListView(ListView):
     search_form_class = None
     search_form = None
+
+    # Allow filtering search results
+    filters_form_class = None
+    filters_form = None
 
     def get_search_form_class(self):
         if self.search_form_class is None:
             raise ImproperlyConfigured("search_form_class is missing")
         return self.search_form_class
-        
-    def get_search_filters(self):
-        filters = {}
-        search_form_class = self.get_search_form_class()
 
-        if self.request.GET:
-            search_form= search_form_class(self.request.GET) # Bind the form to the GET Parameters
-            self.search_form = search_form
-            filters = search_form.get_filters()
-            self.filters = filters
+    def get_filters(self):
+        filters = {}
+        if self.filters_form_class is not None:
+            filters_form = self.filters_form_class(self.request.GET)
+
+            self.filters_form = filters_form
+
+            filters = filters_form.get_filters()
         return filters
+        
+    def get_predicates(self):
+        predicates = {}
+        search_form_class = self.get_search_form_class()
+        search_form = search_form_class(self.request.GET) # Bind the form to the GET Parameters
+
+        self.search_form = search_form
+        
+        predicates = search_form.get_predicates()
+        self.predicates = predicates
+        return predicates
 
     def get_queryset(self):
-        search_filters = self.get_search_filters()
-        field_filters = {}
-        # If field filters have also been applied
-        if self.filters_form_class is not None:
-            field_filters = super().get_filters() 
+        predicates = self.get_predicates()
+        filters = self.get_filters()
+        qs = super(SearchListView, self).get_queryset().filter(**predicates, **filters)
+        return qs
 
-        return super().get_queryset().filter(**search_filters, **field_filters)
 
-    def get_filters_string(self):
-        filters_list = []
-        filters_string = ""
-        if self.search_form:
-            search_form_fields = self.search_form.declared_fields
-            for key, value in self.request.GET.items():
-                # Exclude GET parameters that are not part of the filters form fields
-                if key in search_form_fields:
-                    filters_list.append("{}={}".format(key, value))
-        filters_string = "&".join(filters_list)
-        return filters_string
+    def get_request_parameter_string(self):
+        request_parameter_string = self.search_form.get_request_parameter_string()
+
+        if self.filters_form is not None:
+            request_parameter_string_for_filters = self.filters_form.get_request_parameter_string()
+            if request_parameter_string_for_filters:
+                request_parameter_string = "{}&{}".format(request_parameter_string, request_parameter_string_for_filters)
+        return request_parameter_string
+
         
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.search_form is None:
-            # Form has not been bounded, return Unbound Form
-            context["search_form"] = self.get_search_form_class()
-        else:
-            context["search_form"] = self.search_form
+        context = super(SearchListView, self).get_context_data(**kwargs)
 
-        filters_string = self.get_filters_string()
-        if super().get_filters_string():
-            filters_string = "{}&{}".format(filters_string, super().get_filters_string())
+        filters_form = None
+
+        if self.filters_form:
+            filters_form = self.filters_form
+
+        context["search_form"] = self.search_form
+        context["filters_form"] = filters_form
+        context["request_parameter_string"] = self.get_request_parameter_string()
         
-        context["filters_string"] = filters_string
         return context
