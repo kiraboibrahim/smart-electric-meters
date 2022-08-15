9b88ef3caa1462fda909d70d60706e10b587c7cd
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-08-15 22:17:10 +0300

    refactor: Add function to provide the context data for the list_meters template because a couple of views use this template and thus end up duplicating the get_context_data() method

--- meter/utils.py
+++ meter/utils.py
@@ -1,13 +1,31 @@
-from meter.models import Meter
+import meter.models as meter_models
+import meter.forms as meter_forms
+
 from user.account_types import MANAGER
+
 
 def get_meter_manufacturer_hash(manufacturer_name):
     return manufacturer_name.title().strip().replace(" ", "")
 
 
 def get_meters(request):
-    queryset = Meter.objects.all()
+    queryset = meter_models.Meter.objects.all()
     if request.user.account_type == MANAGER:
         queryset = queryset.filter(manager=request.user.id)
         
     return queryset
+
+
+def get_list_meters_template_context_data(request, base_context={}):
+    # The list_meters templates needs the following objects
+    add_meter_form = meter_forms.AddMeterForm()
+    add_meter_category_form = meter_forms.AddMeterCategoryForm()
+    search_form = meter_forms.SearchForm()
+    meters = get_meters(request)
+        
+    base_context.setdefault("add_meter_form", add_meter_form)
+    base_context.setdefault("add_meter_category_form", add_meter_category_form)
+    base_context.setdefault("search_form", search_form)
+    base_context.setdefault("meters", meters)
+        
+    return base_context
