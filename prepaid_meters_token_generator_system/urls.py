b01724b88274628c974690941e3f9f2239778f0a
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-08-29 08:40:03 +0300

    fix: Remove django.conf.urls.url() because it is deprecated in django 4.0

--- prepaid_meters_token_generator_system/urls.py
+++ prepaid_meters_token_generator_system/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import url
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="login"), name="index"),
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
     path("meters/", include("meter.urls")), 
     path("users/", include("user.urls"))
 ]
